e60161727ff8866c84639bb11c63d90b8f381ca4
Ravichandra N <67008019+ravic906@users.noreply.github.com>
2025-07-19 08:10:55 +0530

    Update offers.py
    Updated code

--- offers.py
+++ offers.py
@@ -10,12 +10,12 @@ old_text = None
 print('Scraping started at:',datetime.now())
 with TelegramClient('newestone', api_id, api_hash,timeout=3600) as client:    
     while 1:
-    	if int(datetime.now().strftime('%M'))%15==0:
+    	if int(datetime.now().strftime('%M'))%10==0:
 	        html_doc = requests.get('https://www.desidime.com/new').text
 	        bs = BeautifulSoup(html_doc,'html.parser')
 	        x=bs.findAll('div',"deal-url",limit=5)            
 	        i=x[0]
-	        if ('soap' in i.text.lower() or 'dettol' in i.text.lower()  or 'bottle' in i.text.lower()) and i.text!=old_text:
+	        if ('kettle' in i.text.lower() or 'diaper' in i.text.lower() or 'toothpaste' in i.text.lower()) and i.text!=old_text:
 	            old_text=i.text
 	            k=k+1
 	            link=i.find('a').get('href')
